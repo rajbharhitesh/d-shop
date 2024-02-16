24ee89af3af1e1b568d4612434c05ce02c4b49b2
Hitesh <h08rajbhar@gmail.com>
2024-02-16 15:58:47 +0530

    install cookieparser

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import colors from 'colors';
 import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 import connectDB from './config/db.js';
 import errorMiddleware from './middlewares/errorMiddleware.js';
 
@@ -24,6 +25,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use('/api/v1', productRoute);
 
