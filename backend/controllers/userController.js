8068f5125045bff35bff78ecb4725c07bd2d7632
Hitesh <h08rajbhar@gmail.com>
2024-02-17 14:27:10 +0530

    update profile controller

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -13,4 +13,23 @@ const getUserProfile = asyncHandler(async (req, res, next) => {
   res.status(200).json({ user });
 });
 
-export { getUserProfile };
+/**-----------------------------------------------
+ * @desc     Update profile by logged in user
+ * @route   /api/v1/users/me/update
+ * @method  PUT
+ * @access  Private
+ ------------------------------------------------*/
+const updateProfile = asyncHandler(async (req, res, next) => {
+  const newUserData = {
+    name: req.body.name,
+    email: req.body.email,
+  };
+
+  const user = await User.findByIdAndUpdate(req.user._id, newUserData, {
+    new: true,
+  });
+
+  res.status(200).json({ user });
+});
+
+export { getUserProfile, updateProfile };
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,10 +1,16 @@
 import express from 'express';
 import { isAuthenticatedUser } from '../middlewares/authMiddleware.js';
-import { getUserProfile } from '../controllers/userController.js';
+import {
+  getUserProfile,
+  updateProfile,
+} from '../controllers/userController.js';
 
 const router = express.Router();
 
 // api/v1/users/me
 router.route('/me').get(isAuthenticatedUser, getUserProfile);
+
+// api/v1/users/me/update
+router.route('/me/update').put(isAuthenticatedUser, updateProfile);
 
 export default router;
