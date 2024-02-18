4960a2fcebd831429cfb372d2fd269992431a988
Hitesh <h08rajbhar@gmail.com>
2024-02-18 12:55:05 +0530

    confirm order page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import UpdatePasswordPage from './pages/user/UpdatePasswordPage';
 import UploadAvatarPage from './pages/user/UploadAvatarPage';
 import CartPage from './pages/cart/CartPage';
 import ShippingPage from './pages/cart/ShippingPage';
+import ConfirmOrderPage from './pages/cart/ConfirmOrderPage';
 
 const App = () => {
   return (
@@ -70,6 +71,15 @@ const App = () => {
               element={
                 <ProtectedRoute>
                   <ShippingPage />
+                </ProtectedRoute>
+              }
+            />
+
+            <Route
+              path="/confirm_order"
+              element={
+                <ProtectedRoute>
+                  <ConfirmOrderPage />
                 </ProtectedRoute>
               }
             />
