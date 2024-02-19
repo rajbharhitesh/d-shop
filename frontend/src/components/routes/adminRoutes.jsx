d21ae5aba1090c37e998ed10ba469af15aeaacef
Hitesh <h08rajbhar@gmail.com>
2024-02-19 20:14:27 +0530

    admin dashboard

--- frontend/src/components/routes/adminRoutes.jsx
+++ frontend/src/components/routes/adminRoutes.jsx
@@ -1,5 +1,20 @@
+import { Route } from 'react-router-dom';
+import ProtectedRoute from '../../components/routes/ProtectedRoute';
+import DashboardPage from '../../pages/admin/DashboardPage';
+
 const adminRoutes = () => {
-  return <></>;
+  return (
+    <>
+      <Route
+        path="/admin/dashboard"
+        element={
+          <ProtectedRoute admin={true}>
+            <DashboardPage />
+          </ProtectedRoute>
+        }
+      />
+    </>
+  );
 };
 
 export default adminRoutes;
